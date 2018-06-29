ed8163e7908296a35dd7caabb92d7833be0ffb5f
DESKTOP-15GUKDC\Kantapon Sritiyot <kantpaon.sritiyot@gmail.com>
2018-06-29 12:10:55 +0700

    Add feature to render data

--- ud989-school-attendance/js/app.js
+++ ud989-school-attendance/js/app.js
@@ -32,4 +32,54 @@
 		},
 	};
 
+	var octopus = {
+		init: function() {
+			attendanceView.init();
+		},
+		getAllAttendances: function() {
+			return model.getAllAttendances();
+		},
+	};
+
+	var attendanceView = {
+		init: function() {
+			this.tbody = document.getElementsByTagName('tbody')[0];
+			this.render();
+		},
+		render: function() {
+			this.tbody.innerHTML = '';
+			octopus.getAllAttendances().map(attendance => {
+				var tr = document.createElement('tr');
+				tr.setAttribute('class', 'student');
+
+				var tdName = document.createElement('td');
+				tdName.setAttribute('class', 'name-col');
+				tdName.textContent = attendance.name;
+				tr.appendChild(tdName);
+
+				var tdDays = attendance.days.map(isAttendanced => {
+					var tdDay = document.createElement('td');
+					tdDay.setAttribute('class', 'attend-col');
+
+					var input = document.createElement('input');
+					input.setAttribute('type', 'checkbox');
+					input.checked = isAttendanced;
+
+					tdDay.appendChild(input);
+					tr.appendChild(tdDay);
+				})
+
+				var tdMissed = document.createElement('td');
+				tdMissed.setAttribute('class', 'missed-col');
+				tdMissed.textContent = attendance.days.reduce(
+					(totalDays, isAttendanced) => totalDays + (isAttendanced ? 0 : 1),
+					0
+				);
+				tr.appendChild(tdMissed);
+				this.tbody.appendChild(tr);
+			});
+		},
+	};
+
+	octopus.init();
 })();
