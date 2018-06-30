d187d612ffd1f7d3c4c63c9ce102e010e90b837f
kanatamon <ponde.moonlord@gmail.com>
2018-06-30 16:04:58 +0700

    Add rendering of `header`

--- frontend-nanodegree-resume/js/resumeBuilder.js
+++ frontend-nanodegree-resume/js/resumeBuilder.js
@@ -1,3 +1,49 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+(function() {
+  var bioModel = {
+    name: 'Kantapon Sritiyot',
+    role: 'Fx Trader, Fullstack Developer, and Traveler',
+    contacts: {
+      mobile: '+668-5529-6837',
+      email: 'kantapon.sritiyot@gmail.com',
+      github: 'kanatamon',
+      location: 'Chiang Mai, Thailand',
+    },
+    welcomMessage: `It's a good day, isn't it?`,
+    skills: [
+      'Fx Trading',
+      'Javascript',
+      'English',
+      'ไทย',
+      '日本語',
+    ],
+    biopic: '../images/fry.jpg',
+  };
+
+  var bioController = {
+    getName: function() { return bioModel.name; },
+    getRole: function() { return bioModel.role; },
+    init: function() {
+      bioView.init();
+    },
+  };
+
+  var bioView = {
+    init: function() {
+      this.headerContainer = document.getElementById('header');
+      this.render();
+    },
+    render: function() {
+      var headerElements = [
+        HTMLheaderName.replace(/%data%/, bioController.getName()),
+        HTMLheaderRole.replace(/%data%/, bioController.getRole()),
+      ];
+
+      this.headerContainer.innerHTML = headerElements.reduce(
+        (done, element) => done + element,
+        ''
+      );
+    },
+  };
+
+  bioController.init();
+})();
